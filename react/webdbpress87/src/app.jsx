970590ef156e3fc8a54ed3ff48a89145b70f7d1b
star__hoshi <kensuke1751@gmail.com>
2016-02-07 22:45:17 +0900

    Flux で Counter を再実装

--- react/webdbpress87/src/app.jsx
+++ react/webdbpress87/src/app.jsx
@@ -1,44 +1,73 @@
 var React = require('react');
+var Fluxxor = require('fluxxor');
 
+var constants = {
+  UPDATE_COUNTER: "UPDATE_COUNTER"
+};
+
+// 1 - Store
+var CounterStore = Fluxxor.createStore({
+  initialize: function() {
+    this.counter = 0;
+    this.bindActions(constants.UPDATE_COUNTER, this.onUpdateCounter);
+  },
+  onUpdateCounter: function(payload) {
+    this.counter = this.counter + payload.value;
+    this.emit('change');
+  },
+  getState: function() {
+    return {counter: this.counter};
+  }
+});
+
+// 5- Action
+var actions = {
+  plusCounter: function() {
+    this.dispatch(constants.UPDATE_COUNTER, {value: 1});
+  },
+  minusCounter: function() {
+    this.dispatch(constants.UPDATE_COUNTER, {value: -1});
+  }
+};
+
+var FluxMixin = Fluxxor.FluxMixin(React),
+  StoreWatchMixin = Fluxxor.StoreWatchMixin;
+
+// 7 - view
 var CounterApp = React.createClass({
-  getInitialState: function() {
-    return {counter: 0};
-  },
+  mixins: [
+    FluxMixin, StoreWatchMixin("CounterStore")
+  ],
 
-  handlePlus: function() {
-    this.setState({
-      counter: this.state.counter + 1
-    });
-  },
-  handleMinus: function() {
-    this.setState({
-      counter: this.state.counter - 1
-    });
+  getStateFromFlux: function() {
+    return this.getFlux().store('CounterStore').getState();
   },
   render: function() {
-    return (
-      <div>
-        <Counter value={this.state.counter} onClickPlus={this.handlePlus} onClickMinus={this.handleMinus}/>
-      </div>
-    );
+    return <Counter value={this.state.counter}/>;
   }
 });
 
 var Counter = React.createClass({
+  mixins: [FluxMixin],
+
   propTypes: {
-    value: React.PropTypes.number.isRequired,
-    onClickPlus: React.PropTypes.func.isRequired,
-    onClickMinus: React.PropTypes.func.isRequired
+    value: React.PropTypes.number.isRequired
   },
 
+  onClickPlus: function() {
+    return this.getFlux().actions.plusCounter();
+  },
+  onClickMinus: function() {
+    return this.getFlux().actions.minusCounter();
+  },
   render: function() {
     return (
       <div>
         <span>count:{this.props.value}</span>
-        <button onClick={this.props.onClickPlus}>
+        <button onClick={this.onClickPlus}>
           +1
         </button>
-        <button onClick={this.props.onClickMinus}>
+        <button onClick={this.onClickMinus}>
           -1
         </button>
       </div>
@@ -46,4 +75,9 @@ var Counter = React.createClass({
   }
 });
 
-React.render(<CounterApp/>, document.getElementById('app-container'));
+var stores = {
+  CounterStore: new CounterStore()
+};
+var flux = new Fluxxor.Flux(stores, actions);
+
+React.render(<CounterApp flux={flux}/>, document.getElementById('app-container'));
